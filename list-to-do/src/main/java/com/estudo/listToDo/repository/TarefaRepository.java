a467b6341858694d5694cb656d1bd958e97c31fc
AdelaineDev <adelainesant18@gmail.com>
2022-12-29 23:01:07 -0300

    Refactor: adiciona anotação ao repository

--- list-to-do/src/main/java/com/estudo/listToDo/repository/TarefaRepository.java
+++ list-to-do/src/main/java/com/estudo/listToDo/repository/TarefaRepository.java
@@ -2,6 +2,8 @@ package com.estudo.listToDo.repository;
 
 import com.estudo.listToDo.entity.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 }
