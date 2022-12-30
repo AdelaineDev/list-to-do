2359a009028548ecf6ab9b3f8e3b4114e3a58f21
AdelaineDev <adelainesant18@gmail.com>
2022-12-29 22:23:35 -0300

    build: estrutura projeto

---
+++ README.md
@@ -0,0 +1,10 @@
+# List to do 
+
+Projeto construído com a linguagem java e aplicando as boas práticas, com o intuido de estudar:
+
+- SOLID;
+- Arquitetura Rest;
+- Testes Unitarios;
+- CI
+
+### Diagrama UML
---
+++ list-to-do/src/test/java/com/estudo/listtodo/ListToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.listtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
