3bf1a33eb7c6ab51563bf563e3fb728861e9d20a
AdelaineDev <adelainesant18@gmail.com>
2023-01-01 19:27:09 -0300

    refactor: adiciona atribuitos a entidade

--- list-to-do/src/main/java/com/estudo/listToDo/entity/Tarefa.java
+++ list-to-do/src/main/java/com/estudo/listToDo/entity/Tarefa.java
@@ -1,19 +1,27 @@
 package com.estudo.listToDo.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+import java.time.Instant;
+
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "tb_tarefa")
 public class Tarefa {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String descricao;
+
+    @Column(columnDefinition =  "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createAt;
+
+    @Column(columnDefinition =  "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updateAt;
 }
